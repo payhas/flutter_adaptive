3397acea17bf13149159d467dfa9dfddabae834d
sh.orazow <sh.orazow@payhas.com>
2024-07-18 12:52:12 +0500

    added controller and onchanged to adaptivetextinput

--- libs/flutter_adaptive_core/lib/components/adaptive_text_input.dart
+++ libs/flutter_adaptive_core/lib/components/adaptive_text_input.dart
@@ -1,7 +1,15 @@
 import 'package:flutter_adaptive_core/flutter_adaptive_core.dart';
+import 'package:flutter/widgets.dart';
 
 class AdaptiveTextInput extends AdaptiveWidget {
-  const AdaptiveTextInput({super.key, this.placeholder});
+  const AdaptiveTextInput({
+    super.key,
+    this.placeholder,
+    this.controller,
+    this.onChanged,
+  });
 
   final String? placeholder;
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
 }
--- libs/flutter_adaptive_core/lib/themes/material/widget_builders/material_text_input_builder.dart
+++ libs/flutter_adaptive_core/lib/themes/material/widget_builders/material_text_input_builder.dart
@@ -4,12 +4,14 @@ import 'package:flutter_adaptive_core/flutter_adaptive_core.dart';
 class MaterialTextInputBuilder
     extends AdaptiveWidgetBuilder<AdaptiveTextInput> {
   @override
-  Widget build(BuildContext context, AdaptiveTextInput widget) {
+  Widget build(BuildContext context, AdaptiveTextInput component) {
     return TextField(
       decoration: InputDecoration(
         border: const OutlineInputBorder(),
-        labelText: widget.placeholder,
+        labelText: component.placeholder,
       ),
+      controller: component.controller,
+      onChanged: component.onChanged,
     );
   }
 }
