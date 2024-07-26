cb12712ed8c4373dddcdb0cc1968ffb4da962f11
sh.orazow <sh.orazow@payhas.com>
2024-07-26 17:12:11 +0500

    adding common fields to AdativeApp widget

--- libs/flutter_adaptive_macos_ui/lib/widget_builders/macosui_app_builder.dart
+++ libs/flutter_adaptive_macos_ui/lib/widget_builders/macosui_app_builder.dart
@@ -5,10 +5,44 @@ import 'package:flutter_adaptive_core/flutter_adaptive_core.dart';
 class MacosUIAdaptiveAppBuilder extends AdaptiveWidgetBuilder<AdaptiveApp> {
   @override
   Widget build(BuildContext context, AdaptiveApp component) {
+    MacosThemeData data = MacosTheme.of(context)
+        .copyWith(brightness: component.darkTheme?.brightness);
+
+    var shortCuts = (component.shortcuts is Map<LogicalKeySet, Intent>)
+        ? component.shortcuts as Map<LogicalKeySet, Intent>
+        : null;
+
     return MacosApp(
       theme: component.theme?.build(context),
       home: component.home,
       debugShowCheckedModeBanner: component.debugShowCheckedModeBanner,
+      navigatorKey: component.navigatorKey,
+      routes: component.routes ?? const <String, WidgetBuilder>{},
+      initialRoute: component.initialRoute,
+      onGenerateRoute: component.onGenerateRoute,
+      onGenerateInitialRoutes: component.onGenerateInitialRoutes,
+      onUnknownRoute: component.onUnknownRoute,
+      navigatorObservers:
+          component.navigatorObservers ?? const <NavigatorObserver>[],
+      builder: component.builder,
+      title: component.title,
+      onGenerateTitle: component.onGenerateTitle,
+      color: component.color,
+      locale: component.locale,
+      localizationsDelegates: component.localizationsDelegates,
+      localeListResolutionCallback: component.localeListResolutionCallback,
+      localeResolutionCallback: component.localeResolutionCallback,
+      supportedLocales: component.supportedLocales,
+      showPerformanceOverlay: component.showPerformanceOverlay,
+      checkerboardRasterCacheImages: component.checkerboardRasterCacheImages,
+      checkerboardOffscreenLayers: component.checkerboardOffscreenLayers,
+      showSemanticsDebugger: component.showSemanticsDebugger,
+      shortcuts: shortCuts,
+      actions: component.actions,
+      restorationScopeId: component.restorationScopeId,
+      scrollBehavior: component.scrollBehavior ?? const MacosScrollBehavior(),
+      darkTheme: data,
+      themeMode: component.themeMode,
     );
   }
 }
