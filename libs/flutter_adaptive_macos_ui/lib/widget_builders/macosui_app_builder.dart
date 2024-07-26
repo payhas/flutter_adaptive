76fb01541ceaaaa7b1cc1139cd0c268856410dfa
sh.orazow <sh.orazow@payhas.com>
2024-07-26 16:00:55 +0500

    setting dark theme and thememode

--- libs/flutter_adaptive_macos_ui/lib/widget_builders/macosui_app_builder.dart
+++ libs/flutter_adaptive_macos_ui/lib/widget_builders/macosui_app_builder.dart
@@ -5,8 +5,14 @@ import 'package:flutter_adaptive_core/flutter_adaptive_core.dart';
 class MacosUIAdaptiveAppBuilder extends AdaptiveWidgetBuilder<AdaptiveApp> {
   @override
   Widget build(BuildContext context, AdaptiveApp component) {
+    MacosThemeData data = component.theme?.build(context);
     return MacosApp(
-      theme: component.theme?.build(context),
+      // theme: component.theme?.build(context),
+      theme: MacosThemeData.light(),
+      darkTheme: MacosThemeData.dark(),
+      themeMode: data.brightness == Brightness.light
+          ? ThemeMode.light
+          : ThemeMode.dark,
       home: component.home,
       debugShowCheckedModeBanner: component.debugShowCheckedModeBanner,
     );
